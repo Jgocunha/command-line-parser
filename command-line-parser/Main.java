a6a2d458d01d8c32f33f3cdd7da31781c3dae45b
Jgocunha <gasparcunha19@outlook.pt>
2023-03-06 14:02:14 +0000

    hello world & Args.java & Main.java

---
+++ command-line-parser/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Args arg = new Args("l,p#,d*", args);
+            boolean logging = arg.getBoolean('l');
+            int port = arg.getInt('p');
+            String directory = arg.getString('d');
+            executeApplicaton(logging, port, directory);
+        } catch (ArgsException e) {
+            System.out.printf("Argument error: %s\n", e.errorMessafe());
+        }
+    }
+
+}
